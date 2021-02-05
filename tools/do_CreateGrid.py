6c64a90d8cf399dd97c702baedd9fae84eba6949
fdamo <damoref.sco@gmail.com>
2021-02-05 11:52:35 +0100

    difference alg.

--- tools/do_CreateGrid.py
+++ tools/do_CreateGrid.py
@@ -6,7 +6,7 @@
  Qgis Plugin to compute noise levels
 
                              -------------------
-        begin                : January 2021
+        begin                : February 2019
         copyright            : (C) 2019 by Arpa Piemonte
         email                : s.masera@arpa.piemonte.it
  ***************************************************************************/
@@ -25,12 +25,12 @@ from qgis.core import QgsProject, QgsMapLayerProxyModel
 import processing
 from qgis.PyQt import QtCore
 from PyQt5 import QtGui, uic
-from PyQt5.QtWidgets import QDialog, QApplication
+from PyQt5.QtWidgets import QDialog, QApplication, QFileDialog
 
 import os
 import sys
 
-from . import on_CreateGrid 
+from . import on_CreateGrid , on_Settings
 
 
 ui_path = os.path.join(
@@ -49,34 +49,53 @@ class Dialog(QDialog, FORM_CLASS):
         self.iface = iface 
         self.setupUi(self)
         self.populate_layer()
+        self.outputSave_pushButton.clicked.connect(self.outputFile)
         self.run_pushButton.clicked.connect(self.run)
 
         
-        horizontal_pace = ['10','20','30','40','50']
-        self.horizontalPace_comboBox.clear()
-        for pace in horizontal_pace:
-            self.horizontalPace_comboBox.addItem(pace)
-
-        vertical_pace = ['10','20','30','40','50']
-        self.verticalPace_comboBox.clear()
-        for pace in vertical_pace:
-            self.verticalPace_comboBox.addItem(pace)
-
+        paces = ['10','20','30','40','50']
+        self.Pace_comboBox.clear()
+        for pace in paces:
+            self.Pace_comboBox.addItem(pace)
 
   
     def populate_layer( self ):
 
         self.layer_ComboBox.clear()
-        self.layer_ComboBox.setFilters(QgsMapLayerProxyModel.VectorLayer ) 
+        self.layer_ComboBox.setFilters(QgsMapLayerProxyModel.VectorLayer) 
     
 
+    def outputFile(self):
+
+        self.gridpoint_lineEdit.clear()
+        self.shapefileName = QFileDialog.getSaveFileName(
+            None, 
+            'Open file', 
+            on_Settings.getOneSetting('directory_last') , 
+            "Shapefile (*.shp);;All files (*)")
+
+        if self.shapefileName is None or self.shapefileName == "":
+            return
+            
+        if str.find(self.shapefileName[0],".shp") == -1 and str.find(self.shapefileName[0],".SHP") == -1:
+            self.gridpoint_lineEdit.setText( self.shapefileName[0] + ".shp")
+        else:
+            self.gridpoint_lineEdit.setText( self.shapefileName[0])
+       
+        pathFile = on_Settings.setOneSetting('directory_last', 
+            os.path.dirname(self.gridpoint_lineEdit.text()))
+
+            
     def run(self):
-        
-        layer = self.layer_ComboBox.currentLayer()
-        hpace = int(self.horizontalPace_comboBox.currentText())
-        vpace = int(self.verticalPace_comboBox.currentText())
+               
+        pace = int(self.Pace_comboBox.currentText())
+        buildings_layer = self.layer_ComboBox.currentLayer()
+        buildings_layer_path = buildings_layer.source()
+
+        grid_point_path = self.gridpoint_lineEdit.text()
+        # buildings_layer_path = buildings_layer.source()
         
 
-        on_CreateGrid.creategrid(layer, hpace, vpace)
+        on_CreateGrid.creategrid(pace, buildings_layer_path, grid_point_path)
 
 
--- tools/on_CreateGrid.py
+++ tools/on_CreateGrid.py
@@ -1,52 +1,65 @@
-# -*- coding: utf-8 -*-
-"""
-/***************************************************************************
- opeNoise
-
- Qgis Plugin to compute noise levels
-
-                             -------------------
-        begin                : January 2021
-        copyright            : (C) 2019 by Arpa Piemonte
-        email                : s.masera@arpa.piemonte.it
- ***************************************************************************/
-
-/***************************************************************************
- *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
- *   (at your option) any later version.                                   *
- *                                                                         *
- ***************************************************************************/
-"""
-
-
-from qgis.core import QgsProject
+from qgis.core import (QgsProject, QgsVectorLayer, QgsVectorFileWriter)
+from qgis.utils import iface
 from qgis import processing
 
+import os
+from qgis._core import QgsFeature, QgsField, QgsFields, QgsWkbTypes
+from PyQt5.QtCore import QVariant
 
-def creategrid(layer, hpace, vpace):
+
+
+def creategrid(pace, buildings_layer_path, grid_point_path):
     
     project = QgsProject.instance()
 
-    crs_layer = layer.crs().authid()
+    crs_project = project.crs().authid()
     
-    extent = layer.extent()
+    extent = iface.mapCanvas().extent()
+
     xmax = extent.xMaximum()
     ymax = extent.yMaximum()
     xmin = extent.xMinimum()
     ymin = extent.yMinimum()
 
-    extent_coords = "%f,%f,%f,%f" %(xmin, xmax, ymin, ymax) 
+    extent_coords = "%f,%f,%f,%f" %(xmin, xmax, ymin, ymax)           
 
-    params = { 'CRS' : crs_layer,
-    'EXTENT' : extent_coords, 
-    'HOVERLAY' : 0, 
-    'HSPACING' : hpace, 
-    'OUTPUT' : 'TEMPORARY_OUTPUT', 
-    'TYPE' : 0, 
-    'VOVERLAY' : 0, 
-    'VSPACING' : vpace }
+    params_creategrid = { 'CRS' : crs_project,
+       'EXTENT' : extent_coords, 
+       'HOVERLAY' : 0, 
+       'HSPACING' : pace, 
+       'OUTPUT' : 'TEMPORARY_OUTPUT', 
+       'TYPE' : 0, 
+       'VOVERLAY' : 0, 
+       'VSPACING' : pace }
 
-    processing.runAndLoadResults("native:creategrid", params)
+    result_grid = processing.run("native:creategrid", params_creategrid)
+
+    grid_points = result_grid['OUTPUT']
+
+    buildings_layer_name = os.path.splitext(
+        os.path.basename(buildings_layer_path))[0]
+
+    buildings_layer = QgsVectorLayer(
+        buildings_layer_path, 
+        buildings_layer_name, 
+        "ogr")
+
+    params_difference = { 'INPUT' : grid_points,
+        'OUTPUT' : 'memory:', 
+        'OVERLAY' : buildings_layer }
+    
+    result_difference =  processing.run("native:difference", params_difference)
+
+    difference_output = result_difference['OUTPUT']
+    
+    QgsVectorFileWriter.writeAsVectorFormat(difference_output, 
+                                            grid_point_path, 
+                                            "UTF-8", 
+                                            difference_output.crs(), 
+                                            "ESRI Shapefile")
+    
+    grid_name = os.path.splitext(os.path.basename(grid_point_path))[0]
+    grid = QgsVectorLayer(grid_point_path, str(grid_name), "ogr")
+
+    project.addMapLayer(grid)
+    
